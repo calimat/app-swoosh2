ef45e98c6651a516f0eff8cdbe215fa1e24e95dc
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-01-18 22:56:43 -0500

    First lesson

---
+++ app-swoosh2/app-swoosh2/Views/BorderButton.swift
@@ -0,0 +1,21 @@
+//
+//  BorderButton.swift
+//  app-swoosh2
+//
+//  Created by Ricardo Herrera Petit on 1/18/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class BorderButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
