06bc32c91d2a58d5471c8c0e04ae20352ced586f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-01-22 21:51:04 -0500

    Model build

---
+++ app-swoosh2/app-swoosh2/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh2
+//
+//  Created by Ricardo Herrera Petit on 1/22/18.
+//  Copyright © 2018 Ricardo Herrera Petit. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var sekectedSkillLevel: String?
+    
+}
