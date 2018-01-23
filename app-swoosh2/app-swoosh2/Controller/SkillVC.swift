d172a6f6f69d6331235189fb071a58a49040e73f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2018-01-22 22:01:25 -0500

    Data between segues

--- app-swoosh2/app-swoosh2/Controller/SkillVC.swift
+++ app-swoosh2/app-swoosh2/Controller/SkillVC.swift
@@ -10,9 +10,12 @@ import UIKit
 
 class SkillVC: UIViewController {
 
+    
+    var player:Player!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        print(player.desiredLeague)
         // Do any additional setup after loading the view.
     }
 
@@ -22,14 +25,5 @@ class SkillVC: UIViewController {
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- app-swoosh2/app-swoosh2/Model/Player.swift
+++ app-swoosh2/app-swoosh2/Model/Player.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Player {
-    var desiredLeague: String?
-    var sekectedSkillLevel: String?
+    var desiredLeague: String!
+    var sekectedSkillLevel: String!
     
 }
